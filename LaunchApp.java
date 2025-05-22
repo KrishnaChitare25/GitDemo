b70c8e8b01ef032c1a29cbecefabe070e4016845
Krishna <krishnachitare25@gmail.com>
2025-05-22 12:56:56 +0530

    change done by dev2

--- LaunchApp.java
+++ LaunchApp.java
@@ -2,7 +2,7 @@ class Launch{
 	public static void main(String[] args)
 	{
 		System.out.println("Studying GitHub!");
-		System.out.println("Some changes made!");
+		System.out.println("Some changes made bro!");
 
 	}
 }
